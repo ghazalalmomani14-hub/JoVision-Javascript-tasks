b49e9724777ccf72b2a776af3a02f460361a28f6
ghazalmomani20 <gmmomani20@cit.just.edu.jo>
2026-01-14 12:18:11 +0300

    Add Task7 - live clock

---
+++ Task7/index.js
@@ -0,0 +1,14 @@
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  const time = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("clock").innerText = time;
+}
+updateClock();
+
+setInterval(updateClock, 1000);
